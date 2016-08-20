a6b3cab08fae6dd87e9738822686525ff12f3da0
Jesse Gallegos <gllgsjss@gmail.com>
2016-08-19 18:28:38 -0700

    feat: created script for graph edges

---
+++ Udacity_Project_Technical_Interview/graph_edges.py
@@ -0,0 +1,5 @@
+def question3(*arg, **kwargs):
+    """This script find the minimum \
+    spanning tree that all vertices in a graph with the \
+    smallest possible weight of edges."""
+    return None
