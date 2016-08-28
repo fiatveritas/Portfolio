ac3ac4c925ce3a5eb312565ebbe8a40216e315d5
Jesse Gallegos <gllgsjss@gmail.com>
2016-08-28 15:13:06 -0700

    test: added a few test cases

--- Udacity_Project_Technical_Interview/palindrome.py
+++ Udacity_Project_Technical_Interview/palindrome.py
@@ -4,16 +4,45 @@ import re
 
 def question2(a):
     """This script finds the longest palindromic substring contained in a"""
-    word_holder = str(a)
-    letter_lister = [ letter for letter in word_holder ]
-    first_index = 0
-    last_index = len(word_holder) - 1
+
+    if check_backwards(a):
+        return "True, \"{}\" \nis a palindrome".format(a)
     
-    return None
+    else:
+        return "False, \"{}\" \nis not a palindrome".format(a)
 def check_backwards(a):
+    """This function checks if the word\
+    is already a palindrome."""
+    print a
+
+    word = ''
     reversed_word = ''
-    reversed_word = reversed_word.join(reversed([i for i in a.lower()])
-    if 
 
+    holder = re.split('\W+', a.lower())
+    print holder
 
-print question2('jesse')
+    word = word.join(holder)
+    print word
+
+    reversed_holder = [ i for i in reversed(word) ]
+    print reversed_holder
+
+    reversed_word = reversed_word.join(reversed(word))
+    print reversed_word
+
+    if reversed_word == word:
+        return True
+    else:
+        return False
+
+####Test Cases
+#print question2('Jesse')
+#print question2('Anna')
+#print question2('Are we not drawn onward, we few, drawn onward to new era?')
+#print question2('No demerits tire me, Don.')
+#print question2('112311')
+#print check_backwards('Anna')
+#print check_backwards('Kayak')
+#print check_backwards('Are we not drawn onward, we few, drawn onward to new era?')
+#print check_backwards("A but tuba.")
+#print check_backwards('Jesse')
