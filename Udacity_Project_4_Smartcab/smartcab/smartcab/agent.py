6fa2debe9ce80f014a9114d0c252e66e6f5a3184
Jesse Gallegos <gllgsjss@gmail.com>
2016-10-17 15:54:38 -0700

    feat: update code

--- Udacity_Project_4_Smartcab/smartcab/smartcab/agent.py
+++ Udacity_Project_4_Smartcab/smartcab/smartcab/agent.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 from environment import Agent, Environment
 from planner import RoutePlanner
 from simulator import Simulator
@@ -11,10 +12,18 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.rate = 0.3
+        self.state = None
+        self.q_learn = {}
+        self.runs = 0
+        self.valid_actions = self.env.valid_actions
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
+        self.runs += 1
+        if self.runs >= 100:
+            self.rate = 0
         
 
     def update(self, t):
@@ -24,14 +33,31 @@ class LearningAgent(Agent):
         deadline = self.env.get_deadline(self)
 
         # TODO: Update state
-        
+        self.state = (self.next_waypoint, inputs['light'], inputs['oncoming'], inputs['left'], inputs['right'])
+
+        #initialize q_learn dictionary by passing state vector and setting q = 1 for long term learning
+        for action in self.valid_actions:
+            if (self.state, action) not in self.q_learn:
+                self.q_learn[(self.state, action)] = 1
         # TODO: Select action according to your policy
-        action = random.choice(self.env.valid_actions[1:])
+
+        chances = [self.q_learn[(self.state, None)], self.q_learn[(self.state, 'forward')], self.q_learn[(self.state, 'left')], self.q_learn[(self.state, 'right')]]
+        chances = np.exp(chances) / np.sum(np.exp(chances), axis=0)
+        exploration = max((100 - self.runs) / 100 , 0) # this line of code decides whether smartcab should choose a random action
+                                                        # based on self.trips taken. As more trips are taken the cab will rule out
+                                                        # chance and decide on max values from q_learning.
+        #action = random.choice(self.env.valid_actions[1:]) #random action
+
+        if random.random() < exploration:
+            action = np.random.choice(self.valid_actions, p = chances)
+        else:
+            action = self.valid_actions[np.argmax(chances)]
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
+        self.q_learn[(self.state, action)] = self.rate * reward + (1 - self.rate) * self.q_learn[(self.state, action)]
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
@@ -46,7 +72,7 @@ def run():
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.000001, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=.0000001, display=True)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
